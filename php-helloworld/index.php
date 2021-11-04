c931f784396dfe48e315cd9f50a82590116efaaa
Student User <student@workstation.lab.example.com>
2021-11-04 09:25:29 -0400

    Changed index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming!\n";
+print "We are changing enna changing!\n Changing! \n Changiiiiiing! \n Ca-ching!!!!\n";
 ?>
